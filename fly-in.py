7fdc86e87e66090de9ad0e2e444cb275ef79a1c5
hugo georges <hugo.georges@learner.42.school>
2026-07-03 15:53:01 +0200

    feat: add temporary printing logic to main

--- fly-in.py
+++ fly-in.py
@@ -3,8 +3,8 @@ from srcs.parsing.input_parser import parse_input
 
 def run() -> None:
     test = parse_input("maps/easy/01_linear_path.txt")
-    for line in test:
-        print(line)
+    for attribute in test.__slots__:
+        print(attribute, test.__getattribute__(attribute))
 
 
 if __name__ == "__main__":
