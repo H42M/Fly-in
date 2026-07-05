8a3c21ea37f7d270374dafbd0968abd94948ed66
hugo georges <hugo.georges@learner.42.school>
2026-07-05 20:17:36 +0200

    wip: transform parsed data into objects

--- srcs/parsing/create_map.py
+++ srcs/parsing/create_map.py
@@ -1,9 +1,11 @@
-from srcs.parsing.entities import Map, Hub, Connection
+from srcs.parsing.entities import Map, Hub, Connection, ZoneType
 from srcs.parsing.input_parser import parse_input
 from srcs.parsing.config import Config
 
 
 def create_hub(hub_data: str) -> Hub:
+    if not hub_data:
+        raise ValueError("Missing hub data.")
     data = hub_data.split("[", 1)
     standard_data = data[0]
     metadata = data[1].removesuffix("]")
@@ -11,27 +13,40 @@ def create_hub(hub_data: str) -> Hub:
     data_items = standard_data.split()
     name = data_items[0]
     coordinates = (int(data_items[1]), int(data_items[2]))
-    
-    for item in metadata:
+    color = None
+    zone = None
+    max_drones = None
+
+    for item in metadata.split():
         if item.startswith("color"):
             color = item.removeprefix("color=")
+        if item.startswith("zone"):
+            zonetype = item.removeprefix("zone=")
+            if zonetype in ZoneType:
+                zone = ZoneType[zonetype]
+        if item.startswith("max_drones"):
+            max_drones = int(item.removeprefix("max_drones="))
 
+    if not name or not coordinates:
+        raise ValueError("Missing hub data.")
 
-    return Hub(
-        name=name,
-        coordinates=coordinates,
-        color=color if color else None,
-        
-    )
-    
+    kwargs = {"name": name, "coordinates": coordinates}
+    if color is not None:
+        kwargs["color"] = color
 
+    if zone is not None:
+        kwargs["zone"] = zone
+
+    if max_drones is not None:
+        kwargs["max_drones"] = max_drones
+    return Hub(**kwargs)
 
 
 def create_entities(path: str) -> Map:
     config = parse_input(path)
     hubs = config.hub_list
     connections = config.connection_list
-    create_hub(config.start_hub)
+    print(create_hub(config.start_hub))
 
 
 if __name__ == "__main__":
