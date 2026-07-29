fd5405868578670a9652928f0fabb0b4f439319a
hugo georges <hugo.georges@learner.42.school>
2026-07-29 16:10:23 +0200

    feat(pathfinding): implement reverse Dijkstra and setup dependencies

--- Makefile
+++ Makefile
@@ -1,32 +1,44 @@
 PYTHON = python3
 REQUIREMENTS = requirements.txt
 MAIN = fly-in.py
+VENV = .venv
+VENV_PYTHON = $(VENV)/bin/python
 
+.PHONY: install run visualizer debug clean lint lint-strict
 
-install:
-	$(PYTHON) -m pip install -r REQUIREMENTS
+$(VENV_PYTHON):
+	$(PYTHON) -m venv $(VENV)
 
-run:
-	@$(PYTHON) $(MAIN)
+install: $(VENV_PYTHON)
+	$(VENV_PYTHON) -m pip install -r $(REQUIREMENTS)
 
-debug:
-	$(PYTHON) -m pdb $(MAIN)
+run: install
+	@$(VENV_PYTHON) $(MAIN)
+
+visualizer: install
+	$(VENV_PYTHON) -m srcs.visualization.dijkstra_visualizer $(MAP)
+
+debug: install
+	$(VENV_PYTHON) -m pdb $(MAIN)
 
 clean:
 	@find . -type d -name "__pycache__" -exec rm -rf {} +
 	@rm -rf .mypy_cache .pytest_cache
 
+fclean: clean
+	rm -rf $(VENV)
+
 lint:
-	@flake8 .
-	@mypy . --warn-return-any \
+	@$(VENV_PYTHON) flake8 .
+	@$(VENV_PYTHON) mypy . --warn-return-any \
 	--warn-unused-ignores \
 	--ignore-missing-imports \
 	--disallow-untyped-defs \
 	--check-untyped-defs
 
 lint-strict:
-	@flake8 .
-	@mypy . --strict \
+	@$(VENV_PYTHON) flake8 .
+	@$(VENV_PYTHON) mypy . --strict \
 	--warn-return-any \
 	--warn-unused-ignores \
 	--ignore-missing-imports \
--- srcs/algorithms/dijkstra.py
+++ srcs/algorithms/dijkstra.py
@@ -1,15 +1,70 @@
 from math import inf
 import heapq
 
-from srcs.parsing.entities import Map
+from srcs.parsing.entities import Map, Hub, ZoneType
 
 
-def algo(map: Map) -> None:
+def find_hub(map: Map, hub_name: str) -> Hub | None:
+    for hub in [map.start_hub, *map.hubs, map.end_hub]:
+        if hub.name == hub_name:
+            return hub
+    return None
+
+
+def algo(map: Map) -> dict[str, float]:
     hubs = [map.start_hub, *map.hubs]
     distances: dict[str, float] = {hub.name: inf for hub in hubs}
     distances[map.end_hub.name] = 0
 
     processed: list[str] = []
-    frontier: list[tuple[float, str]] = [(0, map.end_hub.name),]
+    frontier: list[tuple[float, str]] = [
+        (0, map.end_hub.name),
+    ]
     not_discovered = hubs.copy()
     del hubs
+
+    while frontier:
+        current_distance, current_name = heapq.heappop(frontier)
+
+        if distances[current_name] < current_distance:
+            continue
+
+        candidates = map.neighbours[current_name]
+        c_distances: dict[str, float] = {}
+        hub = find_hub(map, current_name)
+
+        for c in candidates:
+            if c.name1 == current_name:
+                c_name = c.name2
+            else:
+                c_name = c.name1
+
+            if hub is None:
+                continue
+
+            candidate_hub = find_hub(map, c_name)
+
+            if not candidate_hub:
+                continue
+
+            if candidate_hub.zone == ZoneType.BLOCKED:
+                continue
+            elif hub.zone == ZoneType.RESTRICTED:
+                c_distances[c_name] = current_distance + 2
+            else:
+                c_distances[c_name] = current_distance + 1
+
+        for can_name in c_distances:
+            if distances[can_name] > c_distances[can_name]:
+                distances[can_name] = c_distances[can_name]
+                heapq.heappush(
+                    frontier,
+                    (distances[can_name], can_name),
+                )
+
+        processed.append(current_name)
+
+        if hub in not_discovered:
+            not_discovered.remove(hub)
+
+    return distances
